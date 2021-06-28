39ab6c4a3cc827869287e89341ab37c1d1b89b65
Sagnik Sasmal <16162562+SagnikSRHUSE@users.noreply.github.com>
2021-06-29 01:49:51 +0530

    Update entrypoint.sh

--- entrypoint.sh
+++ entrypoint.sh
@@ -6,5 +6,5 @@ MODIFIED_STARTUP=`eval echo $(echo ${STARTUP} | sed -e 's/{{/${/g' -e 's/}}/}/g'
 
 # Run the Server
 /usr/local/bin/python3.9 -m pip install pip==20.3.4
-/usr/local/bin/python3.9 -m pip install -r requirements.txt
+/usr/local/bin/python3.9 -m pip install --no-cache-dir -r requirements.txt
 ${MODIFIED_STARTUP}
