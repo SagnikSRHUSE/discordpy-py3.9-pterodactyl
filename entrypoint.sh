e6ec929aa8b653c4ac0906d19df7140cdad3da01
Sagnik Sasmal <16162562+SagnikSRHUSE@users.noreply.github.com>
2021-06-29 02:10:07 +0530

    Update entrypoint.sh

--- entrypoint.sh
+++ entrypoint.sh
@@ -4,7 +4,11 @@ sleep 2
 # Make tmp dir
 export TMPDIR=/home/container/tmp/
 
-mkdir /home/container/tmp/
+if [ -d "/home/container/tmp/" ] 
+then
+    mkdir /home/container/tmp/
+fi
+
 cd /home/container
 MODIFIED_STARTUP=`eval echo $(echo ${STARTUP} | sed -e 's/{{/${/g' -e 's/}}/}/g')`
 
